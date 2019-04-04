4e28fb2bbfdee8476572ffa04eda5c22eb52f577
Shobi P P <ppshobi@gmail.com>
2019-04-04 16:16:49 +0530

    test count command and refactor pop command tests

--- tests/Unit/IngestChannelTest.php
+++ tests/Unit/IngestChannelTest.php
@@ -5,6 +5,7 @@
  */
 namespace Tests\Unit;
 
+use Psonic\Concretes\Channels\Control;
 use Psonic\Concretes\Channels\Ingest;
 use Psonic\Concretes\Client;
 use Tests\TestCase;
@@ -14,6 +15,7 @@ class IngestChannelTest extends TestCase
     public function setUp(): void
     {
         $this->ingest = new Ingest(new Client());
+        $this->control = new Control(new Client());
     }
 
     /**
@@ -45,12 +47,44 @@ class IngestChannelTest extends TestCase
     public function it_can_pop_items_from_index()
     {
         $this->ingest->connect();
-        echo $this->ingest->flushc($this->collection);
+        $this->control->connect();
+
+        $this->ingest->flushc($this->collection);
         $this->assertEquals(0, $this->ingest->count($this->collection, $this->bucket));
         $this->assertEquals("OK", $this->ingest->push($this->collection, $this->bucket, "1234", "hi Shobi how are you")->getStatus());
+        $this->control->consolidate();
+
+        $this->assertEquals(1, $this->ingest->count($this->collection, $this->bucket));
+
         $this->assertEquals(1, $this->ingest->pop($this->collection, $this->bucket, "1234", "hi Shobi how are you"));
-        $this->assertEquals(0, $this->ingest->count($this->collection, $this->bucket));
+        $this->control->consolidate();
+        $this->assertEquals(0, $this->ingest->count($this->collection, $this->bucket, "1234"));
     }
 
+    /**
+     * @test
+     *
+     **/
+    public function it_can_count_the_objects()
+    {
+        $this->ingest->connect();
+        $this->control->connect();
+
+        $this->ingest->flushc($this->collection);
+        $this->control->consolidate();
+
+        $this->assertEquals(0, $this->ingest->count($this->collection));
+        $this->assertEquals(0, $this->ingest->count($this->collection, $this->bucket));
+
+        $this->assertEquals("OK", $this->ingest->push($this->collection, $this->bucket, "1234", "hi Shobi how are you?")->getStatus());
+        $this->assertEquals("OK", $this->ingest->push($this->collection, $this->bucket, "1235", "hi Naveen how are you?")->getStatus());
+        $this->assertEquals("OK", $this->ingest->push($this->collection, $this->bucket, "3456", "Hi Jomit How are you?")->getStatus());
+
+        $this->control->consolidate();
+
+        $this->assertEquals(3, $this->ingest->count($this->collection, $this->bucket));
+    }
+
+    //TODO: separate tests for flushc, flushb, flusho commands
 
 }
