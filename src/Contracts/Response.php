d0159a9b8df94ec93d61d3d8eded5ea616d9ff0c
Shobi P P <ppshobi@gmail.com>
2019-04-09 00:36:32 +0530

    added get method in the contract

--- src/Contracts/Response.php
+++ src/Contracts/Response.php
@@ -2,4 +2,11 @@
 namespace Psonic\Contracts;
 
 interface Response
-{ }
+{
+    /**
+     * fetches an item from the parsed buffer
+     * @param $key
+     * @return mixed
+     */
+    public function get($key);
+}
