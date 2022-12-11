8d50eac70c2bc0d6111bb596f895723ac4637579
ppshobi <8536607+ppshobi@users.noreply.github.com>
2022-12-11 17:14:03 +0100

    support php 8 and typehints on abstract command

--- src/Commands/Command.php
+++ src/Commands/Command.php
@@ -6,10 +6,16 @@ use Psonic\Contracts\Command as CommandInterface;
 
 abstract class Command implements CommandInterface
 {
-    private $command;
-    private $parameters;
+    private string $command;
+    /**
+     * @var array<mixed>
+     */
+    private array $parameters;
 
-    public function __construct($command, $parameters = [])
+    /**
+     * @param array<mixed> $parameters
+     */
+    public function __construct(string $command, array $parameters = [])
     {
         $this->command = $command;
         $this->parameters = $parameters;
@@ -18,8 +24,9 @@ abstract class Command implements CommandInterface
     /**
      * Wrap the string in quotes, and normalize whitespace. Also remove double quotes.
      */
-    protected function wrapInQuotes($string)
+    protected function wrapInQuotes(string $string):string
     {
+        /** @var string $string */
         $string = preg_replace('/[\r\n\t"]/', ' ', $string);
         $string = '"' . str_replace('"', '\"', $string) . '"';
         return $string;
